dcafdd6c17866be3bc08c7db9170278ffaa87aef
Subham Negi <negisubham210@gmail.com>
2026-08-09 11:05:41 +0530

    commit

--- 36Forms.js
+++ 36Forms.js
@@ -1,5 +1,15 @@
 const form =  document.querySelector('form')
 
-form.addEventListener('click', (event)=>{
-    console.log(event.target.id)
-})
+// form.addEventListener('click', (event)=>{
+//     console.log(event.target.id)
+// })
+
+
+// form.addEventListener('input', (event)=>{
+//     console.log(event.target.value)
+// })
+
+
+// form.addEventListener('submit', (event)=>{
+//     console.log("form submitted")
+// })
