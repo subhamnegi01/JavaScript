124196bf9eaa430b75b38cec35a2f47d4662e6d0
Subham Negi <negisubham210@gmail.com>
2026-08-19 08:59:35 +0530

    update

--- 39Call_Back_Hell.js
+++ 39Call_Back_Hell.js
@@ -1,25 +1,32 @@
-function placeOrder(order) {
+callback = ()=>{
+    preparingOrder()
+}
+
+function placeOrder(callback) {
     console.log("Talking with Dominos's")
 
     setTimeout(() => {
         console.log("Order placed successfully")
+        callback()
     }, 2000)
 }
 
-function preparingOrder(){
+function preparingOrder(callback) {
     console.log("Preparing your order...")
 
     setTimeout(() => {
         console.log("Pizza Preparation is Done.")
+        callback()
     }, 5000)
 }
 
 
-function pickupOrder(){
+function pickupOrder(callback) {
     console.log("Reaching retaurant to pickup your order...")
 
     setTimeout(() => {
         console.log("Order picked up successfully")
+        callback()
     }, 3000)
 }
 
@@ -30,3 +37,15 @@ function deliverOrder(){
         console.log("Order delivered successfully")
     }, 50000)
 }
+
+
+
+placeOrder(()=>{
+    preparingOrder(()=>{
+        pickupOrder(()=>{
+            deliverOrder(()=>{
+                
+            })
+        })
+    })
+})
