4e39263e23681d9ff250bed046ba47b8cc90d341
Subham Negi <negisubham210@gmail.com>
2026-08-19 08:27:51 +0530

    update

---
+++ 38Call_Back_Fun.js
@@ -0,0 +1,59 @@
+// Call back hell
+
+
+// callback function
+// function fetchuser(){
+//     console.log("Fetching the user detail...")
+//     setTimeout(()=>{
+//         console.log("Data fetched successfully")
+//         const obj = {
+//             name: "John",
+//             age: 23,
+//             city: "New York"
+//         }
+//         meet(obj)
+//     }, 2000)
+// }
+
+// function fetchuser1(){
+//     console.log("Fetching the user detail...")
+//     setTimeout(()=>{
+//         console.log("Data fetched successfully")
+//         const obj = {
+//             name: "John",
+//             age: 23,
+//             city: "New York"
+//         }
+//         printAge(obj)
+//     }, 2000)
+// }
+
+function greet(obj){
+    console.log(`Hello ${obj.name}`)
+}
+function meet(obj){
+    console.log(`Hello ${obj.name}, nice to meet you!`)
+}
+
+function printAge(obj){
+    console.log(`user ${obj.age} years old`)
+}
+
+// fetchuser(greet)
+// fetchuser1()
+
+
+function fetchuser(callback){
+    console.log("Fetched info of the user...")
+    setTimeout(()=>{
+        console.log("Data fetched successfully")
+        const obj = {
+            name: "John",
+            age: 23,
+            city: "New York"
+        }
+        callback(obj)
+    }, 2000)
+}
+
+fetchuser(meet)
