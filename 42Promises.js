88317179ed2b23e08363ebb7207083b039bdeccc
Subham Negi <negisubham210@gmail.com>
2026-08-23 22:44:16 +0530

    commit

--- 42Promises.js
+++ 42Promises.js
@@ -1,14 +1,10 @@
 // async task
 // weather
 
+
 const Promises =  fetch('https://api.weatherapi.com/v1/current.json?key=c690c2b4b7b746f895833500262208&q=London&aqi=no')
 
 const pro2 = Promises.then((response)=>{
-    // const pro2 = response.json()
-
-    // pro2.then((data)=>{
-    //     console.log(data)
-    // })
     return response.json()
 })
 
