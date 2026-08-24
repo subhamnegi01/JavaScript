1858700e72be5671ddfe2bd6b72b05ecdaf38f4f
Subham Negi <negisubham210@gmail.com>
2026-08-24 13:32:11 +0530

    update

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,1 +1,14 @@
-document.querySelector('button').addEventListener('click')
+document.querySelector('button').addEventListener('click',()=>{
+
+    const place =  document.getElementById('location').value
+
+    function updateTemp(data){
+        const element = document.getElementById('weatherInfo')
+        element.innerHTML = `Todays temeperature in ${place} is ${data.current.temp_c}°C`
+    }
+    const prom = fetch(`https://api.weatherapi.com/v1/current.json?key=c690c2b4b7b746f895833500262208&q=${place}&aqi=no`)
+    prom
+    .then(response=>response.json())
+    .then(data=>updateTemp(data))
+
+})
