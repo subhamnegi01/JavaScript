89e55870abcd5932ccb448678611eff86d0ba319
Subham Negi <negisubham210@gmail.com>
2026-08-25 09:05:06 +0530

    update

--- Async Await/script.js
+++ Async Await/script.js
@@ -1,76 +1,87 @@
-function placeOrder(cart){
-    console.log("Talking with Domino's");
+// function placeOrder(cart){
+//     console.log("Talking with Domino's");
     
-    const pr = new Promise(function(resolve,reject){
+//     const pr = new Promise(function(resolve,reject){
 
-        setTimeout(()=>{
+//         setTimeout(()=>{
             
-            const food_available = true;
-            if(food_available){
-            console.log("Order Placed Succesfully");
-            const order = {orderId: 221 , food: cart , restaurant: "Dominos", location:"Dwarka"}
-            resolve(order);
-            }
-            else{
-                reject("Items Out of Stocks");
-            }
-            },2000)
+//             const food_available = true;
+//             if(food_available){
+//             console.log("Order Placed Succesfully");
+//             const order = {orderId: 221 , food: cart , restaurant: "Dominos", location:"Dwarka"}
+//             resolve(order);
+//             }
+//             else{
+//                 reject("Items Out of Stocks");
+//             }
+//             },2000)
 
-    })
+//     })
 
 
-    return pr;
-}
+//     return pr;
+// }
 
-function preparingOrder(order){
-    console.log("Pizza preparation started....");
+// function preparingOrder(order){
+//     console.log("Pizza preparation started....");
 
-    const pr = new Promise(function(resolve,reject){
+//     const pr = new Promise(function(resolve,reject){
 
-        setTimeout(()=>{
-            console.log("Pizza preparation Done");
-            const foodDetails = {token: 12 , restaurant: order.restaurant, location: order.location};
-            resolve(foodDetails);
-           },5000)
-    })
+//         setTimeout(()=>{
+//             console.log("Pizza preparation Done");
+//             const foodDetails = {token: 12 , restaurant: order.restaurant, location: order.location};
+//             resolve(foodDetails);
+//            },5000)
+//     })
 
-    return pr;
-};
+//     return pr;
+// };
 
-function pickupOrder(foodDetails){
-    console.log("Reaching restaurant for picking order")
+// function pickupOrder(foodDetails){
+//     console.log("Reaching restaurant for picking order")
     
-    const pr = new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            console.log("Order picked up by Delivery Boy");
-            const droplocation = foodDetails.location;
-            resolve(droplocation);
-           },3000)
+//     const pr = new Promise(function(resolve,reject){
+//         setTimeout(()=>{
+//             console.log("Order picked up by Delivery Boy");
+//             const droplocation = foodDetails.location;
+//             resolve(droplocation);
+//            },3000)
 
-    })
+//     })
 
-    return pr;
+//     return pr;
     
-}
+// }
 
-function deliverOrder(droplocation){
-    console.log("Delivery boy on the way");
+// function deliverOrder(droplocation){
+//     console.log("Delivery boy on the way");
 
+//     setTimeout(()=>{
+//         console.log("Order Delivered succesfully");
+//     },5000)
+// }
+
+
+// async function greet(){
+
+//     const order = await placeOrder(["Pizza","Coke","Garlic Bread"]);
+//     const foodDetails = await preparingOrder(order);
+//     const droplocation = await pickupOrder(foodDetails);
+//     await deliverOrder(droplocation);
+// }
+
+// greet()
+
+// // placeOrder(cart)
+// // .then(order=>preparingOrder(order))
+// // .then(foodDetails=>pickupOrder(foodDetails))
+// // .then(droplocation=>deliverOrder(droplocation))
+// // .catch(error=>console.log(error));
+
+
+const p1 = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        console.log("Order Delivered succesfully");
+        resolve("Promise 1 resolved");
     },5000)
-}
-
-
-
-
-const order = await placeOrder(["Pizza","Coke","Garlic Bread"]);
-const foodDetails = await preparingOrder(order);
-const droplocation = await pickupOrder(foodDetails);
-await deliverOrder(droplocation);
-
-// placeOrder(cart)
-// .then(order=>preparingOrder(order))
-// .then(foodDetails=>pickupOrder(foodDetails))
-// .then(droplocation=>deliverOrder(droplocation))
-// .catch(error=>console.log(error));
+})
+p1.then((response)=>console.log(response))
