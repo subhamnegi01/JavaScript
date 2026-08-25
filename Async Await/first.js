ccc997aac5bec4b3a880db02231f8de9248684c4
Subham Negi <negisubham210@gmail.com>
2026-08-25 18:19:10 +0530

    commit

--- Async Await/first.js
+++ Async Await/first.js
@@ -1,76 +1,82 @@
-// function placeOrder(cart){
-//     console.log("Talking with Domino's");
+function placeOrder(cart){
+    console.log("Talking with Domino's");
     
-//     const pr = new Promise(function(resolve,reject){
+    const pr = new Promise(function(resolve,reject){
 
-//         setTimeout(()=>{
+        setTimeout(()=>{
             
-//             const food_available = true;
-//             if(food_available){
-//             console.log("Order Placed Succesfully");
-//             const order = {orderId: 221 , food: cart , restaurant: "Dominos", location:"Dwarka"}
-//             resolve(order);
-//             }
-//             else{
-//                 reject("Items Out of Stocks");
-//             }
-//             },2000)
+            const food_available = true;
+            if(food_available){
+            console.log("Order Placed Succesfully");
+            const order = {orderId: 221 , food: cart , restaurant: "Dominos", location:"Dwarka"}
+            resolve(order);
+            }
+            else{
+                reject("Items Out of Stocks");
+            }
+            },2000)
 
-//     })
+    })
 
 
-//     return pr;
-// }
+    return pr;
+}
 
-// function preparingOrder(order){
-//     console.log("Pizza preparation started....");
+function preparingOrder(order){
+    console.log("Pizza preparation started....");
 
-//     const pr = new Promise(function(resolve,reject){
+    const pr = new Promise(function(resolve,reject){
 
-//         setTimeout(()=>{
-//             console.log("Pizza preparation Done");
-//             const foodDetails = {token: 12 , restaurant: order.restaurant, location: order.location};
-//             resolve(foodDetails);
-//            },5000)
-//     })
+        setTimeout(()=>{
+            console.log("Pizza preparation Done");
+            const foodDetails = {token: 12 , restaurant: order.restaurant, location: order.location};
+            resolve(foodDetails);
+           },5000)
+    })
 
-//     return pr;
-// };
+    return pr;
+};
 
-// function pickupOrder(foodDetails){
-//     console.log("Reaching restaurant for picking order")
+function pickupOrder(foodDetails){
+    console.log("Reaching restaurant for picking order")
     
-//     const pr = new Promise(function(resolve,reject){
-//         setTimeout(()=>{
-//             console.log("Order picked up by Delivery Boy");
-//             const droplocation = foodDetails.location;
-//             resolve(droplocation);
-//            },3000)
+    const pr = new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Order picked up by Delivery Boy");
+            const droplocation = foodDetails.location;
+            resolve(droplocation);
+           },3000)
 
-//     })
+    })
 
-//     return pr;
+    return pr;
     
-// }
+}
 
-// function deliverOrder(droplocation){
-//     console.log("Delivery boy on the way");
+function deliverOrder(droplocation){
+    console.log("Delivery boy on the way");
 
-//     setTimeout(()=>{
-//         console.log("Order Delivered succesfully");
-//     },5000)
-// }
+    setTimeout(()=>{
+        console.log("Order Delivered succesfully");
+    },5000)
+}
 
 
-// async function greet(){
+async function greet(){
 
-//     const order = await placeOrder(["Pizza","Coke","Garlic Bread"]);
-//     const foodDetails = await preparingOrder(order);
-//     const droplocation = await pickupOrder(foodDetails);
-//     await deliverOrder(droplocation);
-// }
+    try{
+        const order = await placeOrder(["Pizza","Coke","Garlic Bread"]);
+        const foodDetails = await preparingOrder(order);
+        const droplocation = await pickupOrder(foodDetails);
+        await deliverOrder(droplocation);
+    } 
+    catch(error){
+        console.log(error);
 
-// greet()
+    }
+}
+
+greet()
 
 // // placeOrder(cart)
 // // .then(order=>preparingOrder(order))
@@ -79,34 +85,34 @@
 // // .catch(error=>console.log(error));
 
 
-function test1(){
-    const p1 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Promise 1 resolved");
-    },5000)
-    })
-    return p1;
-}
-// p1.then((response)=>console.log(response))
+// function test1(){
+//     const p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise 1 resolved");
+//     },5000)
+//     })
+//     return p1;
+// }
+// // p1.then((response)=>console.log(response))
 
-function test2(){
-    const p2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Promise 2 resolved");
-    },5000)
-    })
-    return p2;
-}
+// function test2(){
+//     const p2 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise 2 resolved");
+//     },5000)
+//     })
+//     return p2;
+// }
 
-// p1.then(value=>console.log(value))
-// p2.then(value=>console.log(value))
+// // p1.then(value=>console.log(value))
+// // p2.then(value=>console.log(value))
 
-async function greet(){
-    const data1 = await test1();
-    console.log(data1)
+// async function greet(){
+//     const data1 = await test1();
+//     console.log(data1)
 
-    const data2 = await test2();
-    console.log(data2)
-}
+//     const data2 = await test2();
+//     console.log(data2)
+// }
 
-greet()
+// greet()
