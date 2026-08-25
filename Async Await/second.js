5fb2d6d78f6fb2b54dd9e0a43c159aedbb3fd725
Subham Negi <negisubham210@gmail.com>
2026-08-25 18:22:24 +0530

    update

--- Async Await/second.js
+++ Async Await/second.js
@@ -18,10 +18,11 @@ function test2(){
 }
 
 async function greet(){
-    const data1 = await test1();
-    console.log(data1)
 
-    const data2 = await test2();
+    console.log("Hello I greet you")
+
+    const [data1, data2] = await Promise.all([test1(), test2()]);
+    console.log(data1)
     console.log(data2)
 }
 
