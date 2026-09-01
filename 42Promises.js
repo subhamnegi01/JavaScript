bf7380c448d7c0bd27154bda81a89c931971e4b6
Subham Negi <negisubham210@gmail.com>
2026-09-01 22:23:11 +0530

    update

--- 42Promises.js
+++ 42Promises.js
@@ -6,6 +6,7 @@
 // .then(response=>response.json())
 // .then(data=>console.log(data))
 
+
 fetch('https://api.weatherapi.com/v1/current.json?key=c690c2b4b7b746f895833500262208&q=London&aqi=no')
 .then(response=>response.json())
 .then(data=>console.log(data.current.temp_c))
